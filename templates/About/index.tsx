f6dcd1832354bbd8baae8a59832d7cfd1c3167bc
capricorncodes <capricorn.engineering@outlook.com>
2025-05-20 12:57:45 -0400

    Vision

--- templates/About/index.tsx
+++ templates/About/index.tsx
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import Hero from "./Hero";
 import Intro from "./Intro";
 import Values from "./Values";
+import Vision from "./Vision";
 
 const AboutPage = () => {
   return (
@@ -9,6 +10,7 @@ const AboutPage = () => {
       <Hero />
       <Intro />
       <Values />
+      <Vision />
     </Layout>
   );
 };
