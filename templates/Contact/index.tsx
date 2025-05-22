4e2dcad5698ebb347f2e4949e937935acd431760
capricorncodes <capricorn.engineering@outlook.com>
2025-05-22 14:12:09 -0400

    Contact form is done

--- templates/Contact/index.tsx
+++ templates/Contact/index.tsx
@@ -3,11 +3,13 @@ import Hero from "./Hero";
 import Faqs from "../HomePage/Faqs";
 import { faqs } from "@/mocks/faqs";
 import MapView from "./MapView";
+import ContactForm from "./ContactForm";
 
 const ContactPage = () => {
   return (
     <Layout>
       <Hero />
+      <ContactForm />
       <MapView />
       <Faqs section={faqs[0]} />
     </Layout>
